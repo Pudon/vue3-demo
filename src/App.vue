a03eae49af037eadd5a55a9ec87f6710d7337528
Pudon <simple952@163.com>
2022-06-13 18:03:33 +0800

    build:element引入

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+const dealClick = () => {
+  ElMessage.error('消息')
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <el-button @click="dealClick">点击展示消息</el-button>
 </template>
 
 <style>
